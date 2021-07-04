f70e7328d00967c002905a64a4e7a4d5ef3d99a7
josephn90 <49831070+josephn90@users.noreply.github.com>
2021-07-04 17:08:39 -0400

    post data

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const { notes } = require("../../data/notes");
 const fs = require('fs');
+const createNewNote = require('../../lib/notes.js');
 
 router.get("/notes", (req, res) => {
     let results = notes;
@@ -9,10 +10,12 @@ router.get("/notes", (req, res) => {
 });
 
 router.post("/notes", (req, res) => {
+    req.body.id = notes.length.toString();
     let newNote = req.body;
-    console.log(newNote);
-    notes.push(newNote);
+    console.log(notes);
+    createNewNote(newNote, notes);
     res.send(newNote);
+    console.log(notes);
 });
 
 module.exports = router;
